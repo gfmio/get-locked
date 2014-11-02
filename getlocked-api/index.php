9a746010e0a82ee856e50764e8a518a0ff8178c8
Frédérique Mittelstaedt <frederique@frederique-mittelstaedt.com>
2014-11-02 15:43:09 +0000

    API Routing boilerplate

--- getlocked-api/index.php
+++ getlocked-api/index.php
@@ -1,5 +1,6 @@
 <?php
- 
+session_start();
+
 // Load Slim framework
 error_reporting(-1);
 ini_set('display_errors', '1');
@@ -23,6 +24,114 @@ $app->add(new \JsonApiMiddleware());
 //         'msg' => 'msg'
 //     ));
 // });
+
+// User routes
+
+$app->get('/users', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+$app->post('/users', function() use ($app) { 
+    // ...
+    $app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+$app->put('/users', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+$app->delete('/users', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+
+$app->get('/users/current', function() use ($app) {
+	 // ...
+    $app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+$app->post('/users/current', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+$app->put('/users/current', function() use ($app) {
+     // ...
+    $app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+$app->delete('/users/current', function() use ($app) {
+    // ...
+    $app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+
+// Session routes (login, logout)
+
+$app->get('/session', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+$app->post('/session', function() use ($app) {
+	// ...
+    $app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+$app->put('/session', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+$app->delete('/session', function() use ($app) {
+	// ...
+    $app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+
+// Lock routes
+
+$app->get('/locks', function() use ($app) {
+    // ...
+    $app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+$app->post('/locks', function($lock) use ($app) {
+    // ...
+    $app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+$app->put('/locks', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+$app->delete('/locks', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+
+$app->get('/locks/:lock', function() use ($app) {
+    // ...
+    $app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+$app->post('/locks/:lock', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+$app->put('/locks/:lock', function() use ($app) {
+    // ...
+    $app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+$app->delete('/locks/:lock', function() use ($app) {
+    // ...
+    $app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+
+// Log routes
+
+$app->get('/logs/user/:user', function() use ($app) {
+	$app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+$app->post('/users/:user', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+$app->put('/users/:user', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+$app->delete('/users/:user', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+
+$app->get('/logs/lock/:lock', function() use ($app) {
+	$app->render(200,array(
+        'msg' => 'msg'
+    ));
+});
+$app->post('/locks/:lock', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+$app->put('/locks/:lock', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+$app->delete('/locks/:lock', function() use ($app) { $app->render(403, array('msg' => 'forbidden', 'error' => true)); });
+
+// Error routes
  
 $app->error(function (\Exception $e) use ($app) {
 	$app->render(500,array(
